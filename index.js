2eb9059b582c3d7b6572f958edc0c3437403b0e4
MinchulHwang1 <114952919+MinchulHwang1@users.noreply.github.com>
2024-03-25 16:53:22 -0400

    finished Event Listeners video.
    I finished Event Listeners video.
    and add base.html file which can be base of js file

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ console.log('hello world')
 
 //console.log(document)  // document object that creates browser
 
-
 const testEl = document.getElementById('text-el')
 
 testEl.textContent = 'bye bye'
+
+//////////////// in event listener, click and receive event will be learned
+
+testEl.addEventListener('click', () =>{
+    console.log('clicked')              // when the textContent is clicked
+    testEl.innerHTML = "<b>clicked</b>" // the letter witten before 'byebye' will be changed as clicked
+
+})
+
+testEl.addEventListener('mouseover', ()=>{  // when mouse is over the textContent
+    console.log('on')                       // the log will be occurred (in this case - on)
+})
+
+testEl.addEventListener('mouseout', ()=>{    // when mouse is our from the textContent
+    console.log('off')                       // the log will be occurred (in this case - off)
+})
+
+document.addEventListener('scroll', ()=>{
+    const positionY = window.scrollY        // when we scroll the mouse,
+    console.log(positionY)                  // the log record the position of mouse(y)
+})
