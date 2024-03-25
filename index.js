ee6cee83db75875a023386db7cd6697dc8abea4d
MinchulHwang1 <114952919+MinchulHwang1@users.noreply.github.com>
2024-03-25 17:18:14 -0400

    3 different ways of Ajax calls.
    I learned 3 different ways of Ajax calls
    which are jquery, XMLHttpRequest, and fetch

--- index.js
+++ index.js
@@ -26,3 +26,48 @@ document.addEventListener('scroll', ()=>{
     const positionY = window.scrollY        // when we scroll the mouse,
     console.log(positionY)                  // the log record the position of mouse(y)
 })
+
+// GET the data with ajax
+const url = 'https://swapi.dev/api/people/'
+
+
+// 1. jquery ajax method <- this is what we are going to use in this course
+
+$.ajax({                                        // get ajax from url.
+    type: 'GET',
+    url: url,
+    success: function(response){                // if success, record log response
+        console.log('jquery ajax', response)
+    },
+    error: function(error){                     // or record log error
+        console.log(error)
+    }
+})
+
+
+// 2. XMLHttpRequest
+const req = new XMLHttpRequest()
+req.addEventListener('readystatechange', ()=>{
+    if(req.readyState === 4){           // request is completed and response is ready
+                                        // 0 is request is not yet initialized
+                                        // 1 is server connection is created
+                                        // 2 is request has been received
+                                        // 3 is request is processing
+
+        console.log('xhttp', JSON.parse(req.responseText))          // it needs to parse by JSON to readable
+    }
+})
+
+req.open('GET', url)
+req.send()
+// this is request of XML on http, it needs to open and send method
+
+
+// 3. fetch method
+fetch(url)
+.then(resp=>resp.json()).then(data => console.log('fetch', data))
+.catch(err => console.log(err))
+// fetching the url, and then take response to json method
+// if there is error, catch that error 
+
+// other popular : axios library, async await + fetch
