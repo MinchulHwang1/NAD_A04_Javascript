2ef13b22801c6d1909162f73e3f6a37b80431c74
MinchulHwang1 <114952919+MinchulHwang1@users.noreply.github.com>
2024-03-25 16:37:29 -0400

    Finished Document object model.
    I finished document object model video.
    This teach what document object is, and show how it works.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('hello world')
+
+//console.log(document)  // document object that creates browser
+
+
+const testEl = document.getElementById('text-el')
+
+testEl.textContent = 'bye bye'
