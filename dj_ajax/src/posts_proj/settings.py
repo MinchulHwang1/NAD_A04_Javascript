41ff725c7b806c77974d72c42cb39aa569ba77e6
MinchulHwang1 <114952919+MinchulHwang1@users.noreply.github.com>
2024-03-26 16:22:48 -0400

    finish second setting up video
    I finish second setting up video and paste base.html file into src file

--- dj_ajax/src/posts_proj/settings.py
+++ dj_ajax/src/posts_proj/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # our own apps
+    'posts',
+    'profiles',
+    # 3rd party apps
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'posts_proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates' ],  # Based HTML use template directory
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,10 +117,19 @@ USE_I18N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = {
+    BASE_DIR / 'static',
+    BASE_DIR / 'posts' / 'static',
+    BASE_DIR / 'profiles' / 'static',
+}
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
