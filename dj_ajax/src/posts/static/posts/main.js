aa0f76dcaa9519549236a6bee25dd33c023dd46b
MinchulHwang1 <114952919+MinchulHwang1@users.noreply.github.com>
2024-03-26 21:35:35 -0400

    Loading more posts with a button click
    I followed the Loading more posts with a button click video and project processes well.
    re-made functions.js in static folder to do not make an error

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -1,63 +1,87 @@
 
 console.log('hello world')
 
-const helloWorldBox = document.getElementById('hello-world')
+// const helloWorldBox = document.getElementById('hello-world')
 const postsBox = document.getElementById('posts-box')
 const spinnerBox = document.getElementById('spinner-box')
+const loadBtn = document.getElementById('load-btn')
+const endBox = document.getElementById('end-box')
 // helloWorldBox.textContent = 'hello world'
 
 // helloWorldBox.innerHTML = 'hello <b>world</b>'
 
-$.ajax({
-    type: 'GET',
-    url: '/hello-world/',
-    success: function(response){
-        console.log('success', response.text)
-        helloWorldBox.textContent = response.text
-    },
-    error: function(error){
-        console.log('error', error)
-    }
-})
 
-$.ajax({
-    type: 'GET',
-    url: '/data/',
-    success: function(response){
-        console.log(response)
-        // const data = JSON.parse(response.data)
-        // console.log(data)
-        const data = response.data
-        setTimeout(()=>{
-            spinnerBox.classList.add('not-visible')
+// $.ajax({
+//     type: 'GET',
+//     url: '/hello-world/',
+//     success: function(response){
+//         console.log('success', response.text)
+//         helloWorldBox.textContent = response.text
+//     },
+//     error: function(error){
+//         console.log('error', error)
+//     }
+// })
 
-            console.log(data)
-            // how to print on web browser from dictionary
-            data.forEach(el => { // this post box came from main.html
-                //postsBox.innerHTML += `${el.title} - <b>${el.body} </b><br>`
-                postsBox.innerHTML += `
-                    <div class="card mb-2">
-                        <div class="card-body">
-                            <h5 class="card-title">${el.title}</h5>
-                            <p class="card-text">${el.body}</p>
-                        </div>
-                        <div class="card-footer">
-                            <div class="row">
-                                <div class="col-2">   
-                                    <a href="#" class="btn btn-primary">Details</a>
-                                </div>
-                                <div class="col-2">   
-                                    <a href="#" class="btn btn-primary">Like</a>
+
+
+let visible = 3
+
+const getData = () => {     // trigger to get data
+    $.ajax({
+        type: 'GET',
+        url: `/data/${visible}`,
+        success: function(response){
+            console.log(response)
+            // const data = JSON.parse(response.data)
+            // console.log(data)
+            const data = response.data
+            setTimeout(()=>{
+                spinnerBox.classList.add('not-visible')
+
+                console.log(data)
+                // how to print on web browser from dictionary
+                data.forEach(el => { // this post box came from main.html
+                    //postsBox.innerHTML += `${el.title} - <b>${el.body} </b><br>`
+                    postsBox.innerHTML += `
+                        <div class="card mb-2">
+                            <div class="card-body">
+                                <h5 class="card-title">${el.title}</h5>
+                                <p class="card-text">${el.body}</p>
+                            </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-2">   
+                                        <a href="#" class="btn btn-primary">Details</a>
+                                    </div>
+                                    <div class="col-2">   
+                                        <a href="#" class="btn btn-primary">Like</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                `
-            });
-        }, 100)
-        
-    },
-    error: function(error){
-        console.log(error)
-    }
+                    `
+                });
+            }, 100)
+            console.log(response.size)
+            if (response.size === 0){
+                endBox.textContent = 'No posts added yet...'
+            }
+            else if (response.size <= visible){
+                loadBtn.classList.add('not-visible')            // hide loaded post
+                endBox.textContent = 'No more posts to load...' // when there is no post section 
+            }
+        },
+        error: function(error){
+            console.log(error)
+        }
+    })
+}
+
+loadBtn.addEventListener('click', ()=>{
+    spinnerBox.classList.remove('not-visible')
+    visible += 3
+    getData()
 })
+
+getData()
